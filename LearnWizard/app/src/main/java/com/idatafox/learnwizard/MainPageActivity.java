82f5c894d77b83605b35f6cb595b4a97939c90f7
idatafox <wishlong@yahoo.com>
2015-05-17 23:05:47 +0800

    yes ,that's the case.

--- LearnWizard/app/src/main/java/com/idatafox/learnwizard/MainPageActivity.java
+++ LearnWizard/app/src/main/java/com/idatafox/learnwizard/MainPageActivity.java
@@ -1,17 +1,73 @@
 package com.idatafox.learnwizard;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android4devs.slidingtab.SlidingTabLayout;
 
-public class MainPageActivity extends ActionBarActivity {
+
+public class MainPageActivity extends ActionBarActivity implements android.support.v7.app.ActionBar.TabListener{
+
+
+    ViewPager viewPager;
+    ActionBar actionBar;
+    private Toolbar toolbar;
+    SlidingTabLayout tabs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+
+
+        toolbar=(Toolbar)findViewById(R.id.topbar);
+        setSupportActionBar(toolbar);
+
+
+        toolbar.setTitle("hello");
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fi
+
+
+
+
+        viewPager=(ViewPager)findViewById(R.id.pager1);
+        String abc[]={"视频库","下载库"};
+        viewPager.setAdapter(new MyAdapterA(getSupportFragmentManager(),abc,2));
+
+
+
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(viewPager);
+
+
+
     }
 
 
@@ -35,5 +91,63 @@ public class MainPageActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
+
+    }
+}
+
+
+
+class MyAdapterA extends FragmentPagerAdapter {
+
+    CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
+    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+
+    MyAdapterA(FragmentManager fm,CharSequence mTitles[], int mNumbOfTabsumb) {
+        super(fm);
+        this.Titles = mTitles;
+        this.NumbOfTabs = mNumbOfTabsumb;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        Fragment fragment=null;
+        if(position==0)
+        {
+            fragment=new TabFirst();
+        }
+        if(position==1)
+        {
+            fragment=new TabSecond();
+        }
+        return fragment;
+    }
+
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return Titles[position];
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
     }
 }
