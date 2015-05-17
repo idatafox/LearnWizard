2f1001e4fceb6452cbbb113bb478fdb4192f3628
idatafox <wishlong@yahoo.com>
2015-05-17 13:14:01 +0800

    yes ,that's the case.

--- LearnWizard/app/src/main/java/com/idatafox/learnwizard/MainActivity.java
+++ LearnWizard/app/src/main/java/com/idatafox/learnwizard/MainActivity.java
@@ -1,18 +1,82 @@
 package com.idatafox.learnwizard;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
-    String testValue;
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener{
+
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+    private String[] funcs;
+    private ActionBarDrawerToggle drawerListener;
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //drawerlistener
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawerlayout);
+        drawerListener=new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                Toast.makeText(MainActivity.this, "drawer openned", Toast.LENGTH_LONG).show();
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                Toast.makeText(MainActivity.this,"drawer closed",Toast.LENGTH_LONG).show();
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerLayout.setDrawerListener(drawerListener);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerListener.setHomeAsUpIndicator(R.drawable.ic_drawer);
+
+
+
+
+        //second Full ListView
+
+        listView=(ListView)findViewById(R.id.funcList);
+        funcs=getResources().getStringArray(R.array.funcationsNameList);
+
+        listView.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,funcs));
+        listView.setOnItemClickListener(this);
+
+
+
+
+
     }
+
+
+
+
 
 
     @Override
@@ -34,6 +98,58 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+
+        if(drawerListener.onOptionsItemSelected(item))
+        {
+
+
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("idatafox.com", "calling onItemClick...");
+        Toast.makeText(this,funcs[position], Toast.LENGTH_LONG).show();
+        selectItem(position);
+
+    }
+
+    public  void selectItem(int position) {
+
+        listView.setItemChecked(position,true);
+        setTitleA(funcs[position]);
+        if(funcs[position].equals("Liferay在线课堂"))
+        {
+            Log.d("idatafox.com","calling Liferay classroom...");
+           // Intent intent=new Intent(this,LiferayLearnActivity.class);
+           // startActivity(intent);
+        }
+    }
+
+    public void setTitleA(String title){
+
+        getSupportActionBar().setTitle(title);
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerListener.onConfigurationChanged(newConfig);
+    }
+
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerListener.syncState();
+    }
+
 }
